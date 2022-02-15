38a8baf19f3b0a02cf29a2e435a7ac3073bc2b8b
SoledadAzu <solchisina@live.com.ar>
2022-02-15 17:07:23 -0300

    session y cokies

---
+++ src/middlewares/cockieCheck.js
@@ -0,0 +1,8 @@
+function coockie(req,res,next){
+    if(req.cookies.color){
+        req.session.bgColor = req.cookies.color
+        res.locals.bgcolor = req.cookies.color
+    }
+    next()
+}
+module.exports=coockie
